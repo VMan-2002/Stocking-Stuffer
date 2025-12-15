e010336476e7c440373ef0a845894de0ab981ce9
WilsontheWolf <git@shorty.systems>
2025-12-14 23:58:09 -0700

    Squashed commit of the following:
    commit a9e19f44ab01555b841d5eaaa92f207ea62a816c
    Author: Eddy <223344763+realeddyplayz@users.noreply.github.com>
    Date:   Sun Dec 7 23:52:29 2025 -0500
    
        Eddy Rob's Stocking Stuffer

---
+++ localization/en-us/realeddyplayz.lua
@@ -0,0 +1,72 @@
+return {
+    descriptions = {
+        stocking_present = {
+            ["Edward Robinson_stocking_sticker"] = {
+                name = 'Eddy Sticker',
+                text = {
+                    'Convert up to 2 played cards',
+                    'to {C:attention}Aces{} if played hand',
+                    'contains at least {C:attention}2{} Aces',
+                    '{stocking}after{}'
+                }
+            },
+            ["Edward Robinson_stocking_tile"] = {
+                name = 'Magic Scrabble Tile',
+                text = {
+                    '{C:green}#1# in #2#{} chance to convert',
+                    'one card of {C:attention}lowest{} rank',
+                    'to an Ace at {C:attention}end of round{}',
+                    '{stocking}after{}'
+                }
+            },
+            ["Edward Robinson_stocking_nature_valley_dark_chocolate_peanut_and_almond_granola_bar"] = {
+                name = 'Nature Valley Bar',
+                text = {
+                    {
+                        'Once per round, take a bite',
+                        'to {C:attention}destroy{} a random Ace and',
+                        'create a random {C:tarot}Tarot{} card',
+                        '{C:inactive}(Must have room)'
+                    },
+
+                    {
+                        '{C:tarot}#1#{} bite#2# remaining',
+                        '{stocking}usable{}'
+                    }
+                }
+            },
+            ["Edward Robinson_stocking_frame_by_frame"] = {
+                name = 'FRAME! BY! FRAME!!!',
+                text = {
+                    'Scored {C:attention}Aces{} give between',
+                    '{X:mult,C:white}X#1#{} and {X:mult,C:white}X#2#{} Mult,',
+                    'Mult {C:green}randomizes{} for each scored Ace',
+                    '{stocking}after{}'
+                }
+            },
+            ["Edward Robinson_stocking_christmas_card"] = {
+                name = 'Christmas Card',
+                text = {
+                    {
+                        "{C:green}Retriggers{} all scored Aces once",
+                        "{s:0.8,C:inactive}an Eddy Rob Christmas!",
+                        "{stocking}after{}"
+                    },
+                    {
+                        "{C:attention}#1#{}#2#",
+                        "{stocking}usable{}"
+                    }
+                }
+            }
+        },
+        stocking_wrapped_present = {
+            ["Edward Robinson_stocking_present"] = {
+                name = '{V:1}Scrabble...?',
+                text = {
+                    '{C:inactive}A gift from Eddy Rob?!',
+                    '   {C:inactive}this is insane...   '
+                }
+            },
+        }
+    }
+}
