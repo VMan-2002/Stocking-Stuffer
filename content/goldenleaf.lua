d5b3678e2f28c5f5524fb4f2dfc594672a14618e
the-Astra <djs8743@rit.edu>
2025-12-25 23:24:17 -0500

    Fix #104

--- content/goldenleaf.lua
+++ content/goldenleaf.lua
@@ -5,14 +5,14 @@ G.ARGS.LOC_COLOURS.gl_pink = HEX("e462d3")
 G.ARGS.LOC_COLOURS.gl_black = HEX("57638c")
 
 SMODS.Font {
-  key = "emoji",
-  path = "NotoEmoji-Bold.ttf"
+    key = "emoji",
+    path = "NotoEmoji-Bold.ttf"
 }
 
 local display_name = '[REDACTED]Autumn'
 
 SMODS.Atlas({
-    key = display_name..'_presents',
+    key = display_name .. '_presents',
     path = 'present_gl.png',
     px = 71,
     py = 95
@@ -72,56 +72,58 @@ StockingStuffer.Present({
     can_use = function(self, card)
         return to_big(G.GAME.dollars) > to_big(card.ability.extra.rerollprice)
     end,
-	loc_vars = function(self, info_queue, card)
-		local hpt = card.ability.extra
-		local vars = {
+    loc_vars = function(self, info_queue, card)
+        local hpt = card.ability.extra
+        local vars = {
             hpt.mult,
             hpt.rerollprice
-		}            
+        }
         local main_end = {
-                {
-                    n = G.UIT.C,
-                    config = { align = "bm", minh = 0.4 },
-                    nodes = {
-                        {
-                            n = G.UIT.C,
-                            config = { ref_table = card, align = "m", colour = hpt.currentchoice == "Grinch" and G.C.GREEN or HEX("e462d3"), r = 0.05, padding = 0.06 },
-                            nodes = {
-                                { n = G.UIT.T, config = { text = ' ' .. localize('ultimatum_'..string.lower(hpt.currentchoice)) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
-                            }
+            {
+                n = G.UIT.C,
+                config = { align = "bm", minh = 0.4 },
+                nodes = {
+                    {
+                        n = G.UIT.C,
+                        config = { ref_table = card, align = "m", colour = hpt.currentchoice == "Grinch" and G.C.GREEN or HEX("e462d3"), r = 0.05, padding = 0.06 },
+                        nodes = {
+                            { n = G.UIT.T, config = { text = ' ' .. localize('ultimatum_' .. string.lower(hpt.currentchoice)) .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.32 * 0.8 } },
                         }
                     }
                 }
             }
-		return { vars = vars, main_end = main_end}
-	end,
-    use = function(self, card, area, copier) 
+        }
+        return { vars = vars, main_end = main_end }
+    end,
+    use = function(self, card, area, copier)
         local hpt = card.ability.extra
         hpt.currentchoice = random_grinch()
         ease_dollars(-hpt.rerollprice)
         hpt.rerollprice = hpt.rerollprice + 1
         card_eval_status_text(card, 'extra', nil, nil, nil,
-		{ message = localize('ultimatum_'..string.lower(hpt.currentchoice)).."!", colour = hpt.currentchoice == "Grinch" and G.C.GREEN or HEX("e462d3")})
+            { message = localize('ultimatum_' .. string.lower(hpt.currentchoice)) .. "!", colour = hpt.currentchoice ==
+            "Grinch" and G.C.GREEN or HEX("e462d3") })
     end,
     keep_on_use = function(self, card)
         return true
     end,
     calculate = function(self, card, context)
         local hpt = card.ability.extra
-		if context.setting_blind and StockingStuffer.first_calculation then
+        if context.setting_blind and StockingStuffer.first_calculation then
             hpt.stupid_fuck_ass_variable_because_end_of_round_is_stupid = nil
         end
         if context.end_of_round and StockingStuffer.first_calculation then
             if not hpt.stupid_fuck_ass_variable_because_end_of_round_is_stupid then
                 hpt.currentchoice = random_grinch()
                 hpt.rerollprice = math.max(hpt.rerollprice - 1, 1)
-            card_eval_status_text(card, 'extra', nil, nil, nil,
-		    { message = localize('ultimatum_'..string.lower(hpt.currentchoice)).."!", colour = hpt.currentchoice == "Grinch" and G.C.GREEN or HEX("e462d3")})
+                card_eval_status_text(card, 'extra', nil, nil, nil,
+                    { message = localize('ultimatum_' .. string.lower(hpt.currentchoice)) .. "!", colour = hpt
+                    .currentchoice == "Grinch" and G.C.GREEN or HEX("e462d3") })
                 hpt.stupid_fuck_ass_variable_because_end_of_round_is_stupid = true
             end
         end
         if context.joker_main and StockingStuffer.first_calculation then
-            return { mult = hpt.mult * (hpt.currentchoice == "Grinch" and -1 or 1)}
+            return { mult = hpt.mult * (hpt.currentchoice == "Grinch" and -1 or 1) }
         end
     end
 })
@@ -157,7 +159,7 @@ StockingStuffer.Present({
 -- DITTO
 
 local ss_calc_ref = SMODS.current_mod.calculate or function() return end
-SMODS.current_mod.calculate = function (self, context)
+SMODS.current_mod.calculate = function(self, context)
     local ret = ss_calc_ref(self, context)
     if context.end_of_round then
         if not StockingStuffer.GlVars.DittoTransform and StockingStuffer.GlVars.DittoTransform ~= "Fuck!" then
@@ -171,29 +173,37 @@ SMODS.current_mod.calculate = function (self, context)
 end
 SMODS.DrawStep(
     {
-    key = 'ditto',
-    order = -25,
-    func = function(card, layer)
-        if (card.ditto or card.config.center.key == "[REDACTED]Autumn_stocking_ditto") and (card.config.center.discovered or card.bypass_discovery_center) then
-            card.children.particles = card.children.particles or Particles(1, 1, 0, 0, {
-                timer = 0.1, scale = 0.075, initialize = false,
-                speed = 0.25, padding = 1, attach = card,
-                fill = true, colours = {G.C.PURPLE},
-            })
-            local _xOffset = (71 - 80)/2/30
-            local _yOffset = (95 - 80)/2/30
-            local scale_mod = 0.02 * math.sin(1.8*G.TIMERS.REAL)
-            local rotate_mod = 0
-            StockingStuffer.GlVars.ditto_sprite = StockingStuffer.GlVars.ditto_sprite or Sprite(card.T.x + 0, card.T.y + 0, 80, 80, G.ASSET_ATLAS["stocking_gl_ditto_mist"], {x = 0, y = 0})
-            StockingStuffer.GlVars.ditto_sprite.role.draw_major = card
-            StockingStuffer.GlVars.ditto_sprite:draw_shader('dissolve',0, nil, nil, card.children.center,scale_mod, rotate_mod, _xOffset, 0.1 + 0.03 + _yOffset,nil, 0.6)
-            StockingStuffer.GlVars.ditto_sprite:draw_shader('dissolve', nil, nil, nil, card.children.center, scale_mod, rotate_mod, _xOffset, _yOffset)
-        end
-    end,
+        key = 'ditto',
+        order = -25,
+        func = function(card, layer)
+            if (card.ditto or card.config.center.key == "[REDACTED]Autumn_stocking_ditto") and (card.config.center.discovered or card.bypass_discovery_center) then
+                card.children.particles = card.children.particles or Particles(1, 1, 0, 0, {
+                    timer = 0.1,
+                    scale = 0.075,
+                    initialize = false,
+                    speed = 0.25,
+                    padding = 1,
+                    attach = card,
+                    fill = true,
+                    colours = { G.C.PURPLE },
+                })
+                local _xOffset = (71 - 80) / 2 / 30
+                local _yOffset = (95 - 80) / 2 / 30
+                local scale_mod = 0.02 * math.sin(1.8 * G.TIMERS.REAL)
+                local rotate_mod = 0
+                StockingStuffer.GlVars.ditto_sprite = StockingStuffer.GlVars.ditto_sprite or
+                Sprite(card.T.x + 0, card.T.y + 0, 80, 80, G.ASSET_ATLAS["stocking_gl_ditto_mist"], { x = 0, y = 0 })
+                StockingStuffer.GlVars.ditto_sprite.role.draw_major = card
+                StockingStuffer.GlVars.ditto_sprite:draw_shader('dissolve', 0, nil, nil, card.children.center, scale_mod,
+                    rotate_mod, _xOffset, 0.1 + 0.03 + _yOffset, nil, 0.6)
+                StockingStuffer.GlVars.ditto_sprite:draw_shader('dissolve', nil, nil, nil, card.children.center,
+                    scale_mod, rotate_mod, _xOffset, _yOffset)
+            end
+        end,
     }
 )
 local gcu = generate_card_ui
-function generate_card_ui(_c, full_UI_table, specific_vars, card_type, badges, hide_desc, main_start, main_end, card) 
+function generate_card_ui(_c, full_UI_table, specific_vars, card_type, badges, hide_desc, main_start, main_end, card)
     local ret = gcu(_c, full_UI_table, specific_vars, card_type, badges, hide_desc, main_start, main_end, card)
     if card and card.ditto then
         generate_card_ui({
@@ -203,6 +213,7 @@ function generate_card_ui(_c, full_UI_table, specific_vars, card_type, badges, h
     end
     return ret
 end
+
 local gupdate = Game.update
 function Game:update(dt, ...)
     local ret = gupdate(self, dt, ...)
@@ -219,7 +230,7 @@ function Game:update(dt, ...)
                 local transformkey = pseudorandom_element(G.P_CENTER_POOLS.stocking_present, "penis").key
                 G.E_MANAGER:add_event(Event({
                     trigger = 'immediate',
-                    func = function()  
+                    func = function()
                         ditto:flip()
                         play_sound('card1', percent)
                         ditto:juice_up(0.3, 0.3)
@@ -229,7 +240,7 @@ function Game:update(dt, ...)
                 delay(0.1)
                 G.E_MANAGER:add_event(Event({
                     trigger = 'immediate',
-                    func = function()  
+                    func = function()
                         ditto:set_ability(transformkey)
                         return true
                     end
@@ -237,7 +248,7 @@ function Game:update(dt, ...)
                 delay(0.1)
                 G.E_MANAGER:add_event(Event({
                     trigger = 'immediate',
-                    func = function()  
+                    func = function()
                         ditto:flip()
                         play_sound('tarot2', percent, 0.6)
                         ditto:juice_up(0.3, 0.3)
@@ -255,9 +266,9 @@ StockingStuffer.Present({
     developer = display_name,
     key = 'ditto',
     pos = { x = 4, y = 0 },
-	add_to_deck = function (self, card, from_debuff)
+    add_to_deck = function(self, card, from_debuff)
         card.ditto = true
-	end
+    end
 })
 
 local card_save = Card.save
@@ -275,7 +286,7 @@ function Card:load(cardTable, other_card)
 end
 
 -- GIC
-    
+
 local gupdate = Game.update
 function Game:update(dt, ...)
     local ret = gupdate(self, dt, ...)
@@ -293,13 +304,14 @@ StockingStuffer.Present({
 })
 
 -- IMPROVISED PAINTER
-    
+
 local ref = Card.set_ability
 function Card:set_ability(a, ...)
     if a then
         return ref(self, a, ...)
     end
 end
+
 StockingStuffer.Present({
     developer = display_name,
     key = 'improvised_painter',
@@ -308,7 +320,7 @@ StockingStuffer.Present({
     can_use = function(self, card)
         return true
     end,
-    use = function(self, card, area, copier) 
+    use = function(self, card, area, copier)
         local hpt = card.ability.extra
         hpt.state = hpt.state == "paint" and "white" or "paint"
         card:juice_up(0.3, 0.3)
@@ -323,17 +335,18 @@ StockingStuffer.Present({
         }
     },
     loc_vars = function(self, info_queue, card)
-		local hpt = card.ability.extra
-		local vars = {
-            hpt.list_of_enhancements[#hpt.list_of_enhancements] and G.P_CENTERS[hpt.list_of_enhancements[#hpt.list_of_enhancements]].label or localize("k_none"),
+        local hpt = card.ability.extra
+        local vars = {
+            hpt.list_of_enhancements[#hpt.list_of_enhancements] and
+            G.P_CENTERS[hpt.list_of_enhancements[#hpt.list_of_enhancements]].label or localize("k_none"),
             #hpt.list_of_enhancements
-		}            
-        return {key = "[REDACTED]Autumn_stocking_improvised_painter_"..hpt.state, vars = vars }
+        }
+        return { key = "[REDACTED]Autumn_stocking_improvised_painter_" .. hpt.state, vars = vars }
     end,
     calculate = function(self, card, context)
         local hpt = card.ability.extra
         if hpt.state == "paint" then
-        -- paintbrush
+            -- paintbrush
             if StockingStuffer.first_calculation and context.individual and not context.repetition and context.cardarea == G.play and not context.end_of_round then
                 local carde = context.other_card
                 if carde.config.center.key == "c_base" and next(hpt.list_of_enhancements) then
@@ -341,7 +354,7 @@ StockingStuffer.Present({
                     hpt.list_of_enhancements[#hpt.list_of_enhancements] = nil
                     G.E_MANAGER:add_event(Event({
                         trigger = 'immediate',
-                        func = function()  
+                        func = function()
                             carde:flip()
                             play_sound('card1', percent)
                             carde:juice_up(0.3, 0.3)
@@ -350,18 +363,18 @@ StockingStuffer.Present({
                     }))
                     delay(0.1)
                     G.E_MANAGER:add_event(Event({
-                       trigger = 'immediate',
-                        func = function()  
+                        trigger = 'immediate',
+                        func = function()
                             carde:set_ability(ind)
                             return true
                         end
                     }))
                     card_eval_status_text(card, 'extra', nil, nil, nil,
-		            { message = localize('imp_painter_enhance'), colour = G.C.IMPORTANT})
+                        { message = localize('imp_painter_enhance'), colour = G.C.IMPORTANT })
                     delay(0.1)
                     G.E_MANAGER:add_event(Event({
                         trigger = 'immediate',
-                        func = function()  
+                        func = function()
                             carde:flip()
                             play_sound('tarot2', percent, 0.6)
                             carde:juice_up(0.3, 0.3)
@@ -371,39 +384,38 @@ StockingStuffer.Present({
                 end
             end
         else
-        -- whiteout
-            if StockingStuffer.second_calculation and context.individual and not context.repetition and context.cardarea == G.play and not context.end_of_round then
-                local carde = context.other_card
-                if carde.config.center.key ~= "c_base" then
-                    table.insert(hpt.list_of_enhancements, context.other_card.config.center.key)
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'immediate',
-                        func = function()  
-                            carde:flip()
-                            play_sound('card1', percent)
-                            carde:juice_up(0.3, 0.3)
-                            return true
-                        end
-                    }))
-                    delay(0.1)
-                    G.E_MANAGER:add_event(Event({
-                    func = function()
-                        carde:set_ability('c_base', nil, true)
-                        return true
+            -- whiteout
+            if StockingStuffer.second_calculation and context.before and not context.blueprint then
+                for k, carde in pairs(context.scoring_hand) do
+                    if carde.config.center.key ~= "c_base" and not carde.debuff and not carde.vampired then
+                        table.insert(hpt.list_of_enhancements, carde.config.center.key)
+                        carde.vampired = true
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'immediate',
+                            func = function()
+                                carde:flip()
+                                play_sound('card1', percent)
+                                carde:juice_up(0.3, 0.3)
+                                return true
+                            end
+                        }))
+                        delay(0.1)
+                        carde:set_ability(G.P_CENTERS.c_base, nil, true)
+                        card_eval_status_text(card, 'extra', nil, nil, nil,
+                            { message = localize('imp_painter_strip'), colour = G.C.IMPORTANT })
+                        delay(0.1)
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'immediate',
+                            func = function()
+                                carde:flip()
+                                play_sound('tarot2', percent, 0.6)
+                                carde:juice_up(0.3, 0.3)
+                                carde.vampired = nil
+                                return true
+                            end
+                        }))
+                        delay(0.1)
                     end
-                    }))
-                    card_eval_status_text(card, 'extra', nil, nil, nil,
-		            { message = localize('imp_painter_strip'), colour = G.C.IMPORTANT})
-                    delay(0.1)
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'immediate',
-                        func = function()  
-                            carde:flip()
-                            play_sound('tarot2', percent, 0.6)
-                            carde:juice_up(0.3, 0.3)
-                            return true
-                        end
-                    }))
                 end
             end
         end
